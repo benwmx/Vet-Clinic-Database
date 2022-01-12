3887f86c63b091a1cd3b51d49367398b3b2d27a9
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2022-01-12 23:51:31 +0100

    Create visits table

--- schema.sql
+++ schema.sql
@@ -37,4 +37,11 @@ CREATE TABLE specializations (
     vet_id INT REFERENCES vets(id),
     species_id INT REFERENCES species(id),
     PRIMARY KEY (vet_id, species_id)
-)
+);
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    date_of_visit DATE,
+    PRIMARY KEY (animal_id,vet_id)
+);
