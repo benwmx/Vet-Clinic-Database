4d4ae2af44e581af277dec4326d5343f225360f9
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2022-01-12 13:53:04 +0100

    Add transactions

--- data.sql
+++ data.sql
@@ -37,7 +37,7 @@ INSERT INTO animals (
 )
 VALUES ('Devimon', '2017-05-12', 5, TRUE, 11);
 
-*******
+-- Additional Data --
 INSERT INTO animals (
   name,
   date_of_birth,
@@ -52,3 +52,28 @@ VALUES
 ('Angemon', '2005-06-12', 1, TRUE, -45),
 ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
 ('Blossom', '1998-10-13', 3, TRUE, 17);
+
+-- Transactions--
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT species FROM animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT after_deletion;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO after_deletion;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
