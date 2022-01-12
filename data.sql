80e55b87b523aea9bc5d9159a35a38dbd68e9bd4
Rachid Boudaoudi <rachidboudaoudi95@gmail.com>
2022-01-12 18:22:50 +0100

    Add owners/species IDs to animals table.

--- data.sql
+++ data.sql
@@ -74,3 +74,37 @@ INSERT INTO species (
 VALUES 
 ('Pokemon'),
 ('Digimon');
+
+-- Modifications of animals table
+UPDATE animals SET
+species_id = (SELECT id FROM species WHERE name = 'Digimon')
+WHERE name LIKE '%mon';
+
+UPDATE animals SET
+species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+WHERE name NOT LIKE '%mon';
+
+UPDATE animals SET 
+owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+UPDATE animals SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Jennifer Orwell'
+)
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Bob'
+)
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Melody Pond'
+)
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals SET owner_id = (
+  SELECT id FROM owners WHERE full_name = 'Dean Winchester'
+)
+WHERE name IN ('Angemon', 'Boarmon');
+
